db0df290ed53c49bb3c1ea08db3b8677b901a504
mullman <marianullmangithub@gmail.com>
2015-01-04 13:44:57 -0500

    forgot to commit this earlier

---
+++ src/unoGame/gui/PickCardsActionListener.java
@@ -0,0 +1,17 @@
+package unoGame.gui;
+
+import unoGame.Deck;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PickCardsActionListener implements ActionListener {
+
+    public PickCardsActionListener(){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // what happens when a player picks a card
+    }
+}
